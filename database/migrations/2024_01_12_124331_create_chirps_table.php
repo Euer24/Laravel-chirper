0e7a113e66b99a6d3d639bb9e61d17dc4c82f102
Euer <euer2017@gmail.com>
2024-01-12 16:38:10 +0100

    Je kan nu messages versturen

--- database/migrations/2024_01_12_124331_create_chirps_table.php
+++ database/migrations/2024_01_12_124331_create_chirps_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('chirps', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete(); //cascadeOnDelete zorgt ervoor dat als de user verwijderd wordt alle chirps van die user ook verwijderd worden
+            $table->string('message');
             $table->timestamps();
         });
     }
